996930066a69d45f50aff88d68a810e344e36623
Zubair <lakhia@chapman.edu>
2024-05-30 14:17:17 -0700

    Updated code

--- utils/CDW_code_checker.py
+++ utils/CDW_code_checker.py
@@ -8,22 +8,23 @@ warnings.filterwarnings('ignore')
 
 
 #medical codes file
-file_path = r"C:\Users\standard\Desktop\ExcelCodeData\med_codes.xlsx"  # This is the CDW. Make sure an updated version is inside the input folder. 
+file_path = r"codeset/input/med_codes.xlsx"  # This is the CDW. Make sure an updated version is inside the input folder or change the file path to where it is stored. 
 
 sheet_name_icd10_loinc_cpt = "icd10-cpt-loinc"
 sheet_name_ndc_snomed_icd9 = "ndc-snomed-icd9"
 med_codes_icd10_loinc_cpt = pd.read_excel(file_path, sheet_name= sheet_name_icd10_loinc_cpt)
 med_codes_ndc_snomed_icd9 = pd.read_excel(file_path, sheet_name= sheet_name_ndc_snomed_icd9)
 
-#Codes to Check
-file_path_1 = file_path_1 = r"C:\Users\standard\Desktop\ExcelCodeData\arryth_codes.xlsx"  # File with codes to check. Make sure an updated version is in the input folder. There should be a column labeled as 'In CDW' which should be blank.
-#File with codes to check. Make sure an updated version is in the input folder. There should be a column labeled as 'In CDW' which should be blank. 
+#Codes to Check- Enter any medical codes file here. Example file is arrhythmias medical codes.
+file_path_1 = r"codeset/input/arrhythmias_codes.xlsx"  # File with codes to check. Make sure an updated version is in the input folder. There should be a column labeled as 'In CDW' which should be blank.
 sheet_name = "Code Set Details"
-arry_codes = pd.read_excel(file_path_1, sheet_name=sheet_name)
+confirmed_codes = pd.read_excel(file_path_1, sheet_name=sheet_name)
+
 
 #NDC values format adjustment. Uses boolean mask to check if values have the CodeSet of "NDC". Then performs format adjustment using regex.
 is_ndc = med_codes_ndc_snomed_icd9['CodeSet'] == 'NDC'
-med_codes_ndc_snomed_icd9.loc[is_ndc, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ndc, 'NDC'].str[:10]
+med_codes_ndc_snomed_icd9.loc[is_ndc, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ndc, 'NDC'].str[:-3]
+
 
 #ICD-10 values format adjustment. Uses boolean mask to check if values have the CodeSet of "ICD-10". Then performs format adjustment using regex.
 is_ICD_10 = med_codes_icd10_loinc_cpt['CodeSet'] == 'ICD-10'
@@ -32,24 +33,30 @@ med_codes_icd10_loinc_cpt.loc[is_ICD_10, 'Code'] = med_codes_icd10_loinc_cpt.loc
 #ICD-9 values format adjustment. Uses boolean mask to check if values have the CodeSet of "NDC". Then performs format adjustment using regex.
 is_ICD_9 = med_codes_ndc_snomed_icd9['CodeSet'] == 'ICD-9'
 med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'].str.replace(r'^(\d+)$', r'\1.0', regex=True)
-# Remove trailing zeros from decimal values-small but important test case
-med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'].str.replace(r'(\.\d*[1-9])0+$', r'\1', regex=True)
+
+
+
+#Remove trailing zeros from decimal values-small but important test case
+#med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'].str.replace(r'(\.\d*[1-9])0+$', r'\1', regex=True)
 
 
 
 #First if statement in for loop covers keyword checks. Else statement covers NDC, ICD-10, CPT etc., and labels 'Yes' or 'No' in the 'in_cdw' column.
-for (index_cs, code_set_val), (index_code, code_val) in zip(arry_codes["Code Set"].items(), arry_codes["Code"].items()):
+        
+for index_cs, code_set_val in confirmed_codes["Code Set"].items():
     if code_set_val == 'Keyword':
-        temp_desc = arry_codes.at[index_cs, "Code Description"]
+        temp_desc = confirmed_codes.at[index_cs, "Code Description"]
         in_cdw = med_codes_ndc_snomed_icd9['LocalDrugNameWithDose'].str.contains(temp_desc, case =False).any()
-        arry_codes.at[index_cs, "In CDW"] = 'Yes' if in_cdw else 'No'
+        confirmed_codes.at[index_cs, "In CDW"] = 'Yes' if in_cdw else 'No'
 
     else:
-        temp = arry_codes.at[index_code, "Code"]
+        temp = confirmed_codes.at[index_cs, "Code"]
         in_cdw = med_codes_icd10_loinc_cpt['Code'].eq(temp).any() or med_codes_ndc_snomed_icd9['NDC'].eq(temp).any()
-        arry_codes.at[index_code, "In CDW"] = 'Yes' if in_cdw else 'No'
+        confirmed_codes.at[index_cs, "In CDW"] = 'Yes' if in_cdw else 'No'
 
 
-output_file_path = r'C:\Users\standard\Desktop\ExcelCodeData\arry_codes.xlsx'
-arry_codes.to_excel(output_file_path, index=False)
+#Update output file path to desired output file path. A new file 'confirmed_codes' will be generated with values representing if codes are found in CDW. 
+
+output_file_path = r'codeset/output/confirmed_codes.xlsx'
+confirmed_codes.to_excel(output_file_path, index=False)
 
