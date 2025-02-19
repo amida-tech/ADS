efe538400d012334a41766e2cae4169a7d0e4e41
Zubair <lakhia@chapman.edu>
2025-02-19 09:40:42 -0800

    Minor tweaks and updates to CDW Code checker script

--- utils/CDW_code_checker.py
+++ utils/CDW_code_checker.py
@@ -4,7 +4,11 @@ import warnings
 warnings.filterwarnings('ignore')
 
 #This code will cross check whether codes are present in the CDW and label the 'In CDW' column with either 'Yes' or 'No'. Script assumes that there are sheets labeled as icd10-cpt-loinc and ndc-snomed-icd9.
-#Script also assumes that in sheet name icd10-cpt-loinc, there are column names labeled as CodeSet, Code and Description. Sheet name ndc-snomed-icd9 should have columns CodeSet, NDC and LocalDrugNameWithDose.
+#Script also assumes that in sheet name icd10-cpt-loinc, there are column names labeled as CodeSet, Code and Description. Sheet name ndc-snomed-icd9 should also have columns CodeSet, Code and Description.
+
+
+#Codes to Check- Enter any medical codes file here.
+Code_Set_Name = ""
 
 
 #medical codes file
@@ -14,9 +18,10 @@ sheet_name_icd10_loinc_cpt = "icd10-cpt-loinc"
 sheet_name_ndc_snomed_icd9 = "ndc-snomed-icd9"
 med_codes_icd10_loinc_cpt = pd.read_excel(file_path, sheet_name= sheet_name_icd10_loinc_cpt)
 med_codes_ndc_snomed_icd9 = pd.read_excel(file_path, sheet_name= sheet_name_ndc_snomed_icd9)
+med_codes_ndc_snomed_icd9.columns = med_codes_ndc_snomed_icd9.columns.str.replace('NDC', 'Code')
+med_codes_ndc_snomed_icd9.columns = med_codes_ndc_snomed_icd9.columns.str.replace('LocalDrugNameWithDose', 'Description')
 
-#Codes to Check- Enter any medical codes file here.
-Code_Set_Name = "NDC Codes"
+
 
 
 file_path_1 = f"codeset/input/{Code_Set_Name}.xlsx"  # File with codes to check. Make sure an updated version is in the input folder. There should be a column labeled as 'In CDW' which should be blank.
@@ -28,7 +33,7 @@ confirmed_codes['Code'] = confirmed_codes['Code'].astype(str)
 
 #NDC values format adjustment. Uses boolean mask to check if values have the CodeSet of "NDC". Then performs format adjustment using regex.
 is_ndc = med_codes_ndc_snomed_icd9['CodeSet'] == 'NDC'
-med_codes_ndc_snomed_icd9.loc[is_ndc, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ndc, 'NDC'].str[:-3]
+med_codes_ndc_snomed_icd9.loc[is_ndc, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ndc, 'Code'].str[:-3]
 
 
 #ICD-10 values format adjustment. Uses boolean mask to check if values have the CodeSet of "ICD-10". Then performs format adjustment using regex.
@@ -37,7 +42,7 @@ med_codes_icd10_loinc_cpt.loc[is_ICD_10, 'Code'] = med_codes_icd10_loinc_cpt.loc
 
 #ICD-9 values format adjustment. Uses boolean mask to check if values have the CodeSet of "NDC". Then performs format adjustment using regex.
 is_ICD_9 = med_codes_ndc_snomed_icd9['CodeSet'] == 'ICD-9'
-med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'].str.replace(r'^(\d+)$', r'\1.0', regex=True)
+med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'NDC'] = med_codes_ndc_snomed_icd9.loc[is_ICD_9, 'Code'].str.replace(r'^(\d+)$', r'\1.0', regex=True)
 
 #ICD-9 values format adjustment in codes to check file(ex. changes 150 to 150.0)
 for index, row in confirmed_codes.iterrows():
@@ -56,12 +61,15 @@ for index, row in confirmed_codes.iterrows():
 for index_cs, code_set_val in confirmed_codes["Code Set"].items():
     if code_set_val == 'Keyword':
         temp_desc = confirmed_codes.at[index_cs, "Code Description"]
-        in_cdw = med_codes_ndc_snomed_icd9['LocalDrugNameWithDose'].str.contains(temp_desc, case =False).any()
+        in_cdw = med_codes_ndc_snomed_icd9['Description'].str.contains(temp_desc, case =False).any()
         confirmed_codes.at[index_cs, "In CDW"] = 'Yes' if in_cdw else 'No'
+
+    elif code_set_val == 'RxNorm' or code_set_val == 'RXNORM':
+        confirmed_codes.at[index_cs, "In CDW"] = 'No'
 
     else:
         temp = confirmed_codes.at[index_cs, "Code"]
-        in_cdw = med_codes_icd10_loinc_cpt['Code'].eq(temp).any() or med_codes_ndc_snomed_icd9['NDC'].eq(temp).any()
+        in_cdw = med_codes_icd10_loinc_cpt['Code'].eq(temp).any() or med_codes_ndc_snomed_icd9['Code'].eq(temp).any()
         confirmed_codes.at[index_cs, "In CDW"] = 'Yes' if in_cdw else 'No'
 
 
