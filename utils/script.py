9d7ec576ec0f2509287d37d5c4de7cab71766ca9
Zubair <lakhia@chapman.edu>
2025-03-20 13:41:37 -0700

    fixed linting again

---
+++ utils/script.py
@@ -0,0 +1,13 @@
+"""This script calculates the sum of two numbers and prints the result."""
+
+
+def good_func():
+    """Calculate the sum of two numbers and print the result."""
+    a = 1
+    b = 2 + 8
+    c = a + b
+    print("Sum is", c)
+
+
+if __name__ == "__main__":
+    good_func()
